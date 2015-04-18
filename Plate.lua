9cfa985e007826d913643d8ea7b5bb1dc78639b7
Tommy Hansen <tvhansen33@gmail.com>
2015-04-18 18:50:17 +0200

    lol

--- Plate.lua
+++ Plate.lua
@@ -1,18 +1,22 @@
 local Plate = class("Plate", Entity)
 
+local imgSrc = Resources.static:getImage("dirty_plates.png")
+local quad = {}
+for i=1,3 do
+	quad[i] = love.graphics.newQuad((i-1)*16, 0, 16, 16, 48, 16)
+end
+
 function Plate:initialize(x, y, scene)
 	Entity.initialize(self, x, y, scene)
 	self.ox = love.math.random(6)-3
 	self.oy = love.math.random(6)-3
+	self.quad = quad[1]
 end
 
 function Plate:draw(intensity)
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.circle("fill", self.x+self.ox, self.y+self.oy, 8)
-	love.graphics.setColor(0, 0, 0)
-	love.graphics.circle("line", self.x+self.ox, self.y+self.oy, 8)
+	love.graphics.draw(imgSrc, self.quad, self.x+self.ox, self.y+self.oy)
 	love.graphics.setColor(0, 0, 0, intensity)
-	love.graphics.circle("fill", self.x+self.ox, self.y+self.oy, 8)
+	love.graphics.draw(imgSrc, self.quad, self.x+self.ox, self.y+self.oy)
 	love.graphics.setColor(255, 255, 255, 255)
 end
 
