2f6996b103dcb1d2b0c285511dbd17f5cc88b911
Tommy V Hansen <tvhansen33@gmail.com>
2015-04-20 13:50:56 +0200

    added clock

--- Clock.lua
+++ Clock.lua
@@ -2,7 +2,13 @@ local Info = require "Info"
 
 local Clock = class("Clock", Info)
 
-local LIMIT = 10
+local imgSrc = Resources:getImage("clock.png")
+local quad = {}
+for i=0,8 do
+	quad[i] = love.graphics.newQuad(i*16, 0, 16, 16, 144, 16)
+end
+
+local LIMIT = 20
 
 function Clock:initialize(x, y, scene)
 	Info.initialize(self, x, y, scene)
@@ -19,8 +25,8 @@ function Clock:isOut()
 end
 
 function Clock:draw()
-	love.graphics.setColor(255, 255,255 ,255)
-	love.graphics.print(math.floor(self.clock), self.x, self.y)
+	local frame = math.min(8, math.floor(self.clock/(LIMIT/9)))
+	love.graphics.draw(imgSrc, quad[frame], self.x, self.y, 0, self.scale, self.scale)
 end
 
 return Clock
