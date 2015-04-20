d0f875144e1c25b02ca3a91660443678bbc2d712
Mikkel Levisen <mikkel.levisen@gmail.com>
2015-04-20 14:53:04 +0200

    clock shadow

--- Clock.lua
+++ Clock.lua
@@ -3,6 +3,7 @@ local Info = require "Info"
 local Clock = class("Clock", Info)
 
 local imgSrc = Resources:getImage("clock.png")
+local shadow = Resources.static:getImage("shadow.png")
 local quad = {}
 for i=0,8 do
 	quad[i] = love.graphics.newQuad(i*16, 0, 16, 16, 144, 16)
@@ -26,6 +27,7 @@ end
 
 function Clock:draw()
 	local frame = math.min(8, math.floor(self.clock/(LIMIT/9)))
+   love.graphics.draw(shadow,self.x+8,self.y+8,0,0.7,0.7,16,16)
 	love.graphics.draw(imgSrc, quad[frame], self.x, self.y, 0, self.scale, self.scale)
 end
 
